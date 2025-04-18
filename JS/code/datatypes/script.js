ff634ccb69e0429def60ec8122260a36e3bb91d7
prasad-bigdp <bigdp585@gmail.com>
2025-04-18 12:55:47 +0530

    update

---
+++ JS/code/datatypes/script.js
@@ -0,0 +1,29 @@
+let x=2.5252,k=2555695899;
+console.log(typeof x);
+let y = k.toString(16)
+console.log(y)
+console.log(typeof y)
+console.log(typeof x)
+let z = x.toFixed(2)
+console.log(z) //2.53
+// Math Methods
+let a = 64, b = 27;
+console.log(Math.sqrt(a))
+console.log(Math.cbrt(b))
+console.log(Math.max(5, 8, 7, 2, 6, 21))
+console.log(Math.min(5, 8, 7, 2, 6, 21))
+console.log(Math.round(5.24888))
+console.log(Math.ceil(Math.random() * 6));
+console.log(Math.ceil(-3.14));
+console.log(Math.floor(-3.14))
+console.log(Math.abs(-1.41))
+
+
+
+
+
+
+
+
+
+
---
+++ JS/code/variables/script.js
@@ -0,0 +1,40 @@
+// function display ()
+// {
+//     if (true)
+//     {
+//         console.log(a)
+//         var a = 25; //scope is function
+//         var a = 50; //redeclaring
+//         a=25 //reassigning
+//     }
+//     console.log(a)
+// }
+// display()
+
+
+
+// function display2() {
+//     if (true)
+//     {
+//         // console.log(a); -- cannot be used before initialization
+//         let a = 50;
+//         //let a= 25 redclare not possible
+//         a = 25;
+
+// 	}
+// 	// console.log(a) //outside block not accessble
+// }
+// display2()
+
+// JS code to add two numbers and print result in console
+let a = 20, b = 30;
+let c = a + b;
+console.log("The result is "+c)
+
+
+
+
+
+
+
+
