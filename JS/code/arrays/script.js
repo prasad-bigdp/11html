05eb308dcf436085eb73b7fa2fb35128ca5425f3
prasad-bigdp <bigdp585@gmail.com>
2025-04-30 12:44:49 +0530

    update

---
+++ JS/code/arrays/script.js
@@ -0,0 +1,96 @@
+let arr = [5, 4, 6, 9];
+console.log(arr[0] + arr[1]) //5+4 = 9
+//array methods
+console.log(arr.length) //4
+arr[10] = 50;
+console.log(arr.length, arr) //11
+arr = [10, 9, 8, 7]
+arr.push(5,6,1,3)
+console.log(arr) //[10,9,8,7,5,6,1,3]
+arr.pop()
+console.log(arr) // [10,9,8,7,5,6,1]
+arr.unshift(12, 11) 
+console.log(arr) // [12,11,10,9,8,7,5,6,1]
+arr.shift();
+console.log(arr) // [11,10,9,8,7,5,6,1]
+arr.splice(5)
+console.log(arr); //[11,10,9,8,7]
+arr.splice(0,1)
+console.log(arr); //[10,9,8,7]
+arr.splice(1, 2, 15, 16, 17, 18)
+console.log(arr) //[10,15,16,17,18,7]
+arr.reverse();
+console.log(arr) //[7,18,17,16,15,10]
+console.log(arr.indexOf(17)) //2
+console.log([5, 4, 3, 5, 2, 5, 1].lastIndexOf(5))//5
+
+
+
+
+
+
+//forEach
+let arr = [5, 4, 6, 3];
+arr.forEach(function (e,i)
+{
+    console.log(e,i)
+})
+
+let names = "Durga Prasad",str=''
+names.split(' ').forEach(function (v, i)
+{
+    str = str + v.charAt(0).toUpperCase()
+})
+console.log(str) //DP
+
+arr = [1, 2, 3, 4];
+// print sum of array elements using forEach
+let sum = 0;
+arr.forEach(function (v)
+{
+    sum = sum + v;
+})
+console.log(sum);
+arr = [[10, 20], [30, 40]] //input
+//output: [30,70]
+let arr2=[]
+arr.forEach(function (val)
+{
+    console.log(val) // internal array
+    let sum = 0;
+    val.forEach((v) =>
+    {
+        sum = sum + v;
+    })
+    arr2.push(sum)
+})
+console.log(arr2) //[30,70]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- JS/code/functions/script.js
+++ JS/code/functions/script.js
@@ -1,74 +1,221 @@
-function display ()
-{
-    console.log("hello world")
-}
-display()
-// sum of 2 numbers
-function sum ()
-{
-    let a = 10, b = 20;
-    console.log(a + b);
-}
-sum();
-// sum of 2 numbers with parameters
-function sum1 (a, b)
-{
-    console.log(a + b);
-}
-sum1(5, 6);
-sum1("raj", "prasad")
+// function display ()
+// {
+//     console.log("hello world")
+// }
+// display()
+// // sum of 2 numbers
+// function sum ()
+// {
+//     let a = 10, b = 20;
+//     console.log(a + b);
+// }
+// sum();
+// // sum of 2 numbers with parameters
+// function sum1 (a, b)
+// {
+//     console.log(a + b);
+// }
+// sum1(5, 6);
+// sum1("raj", "prasad")
 
 
-function printLen(a)
-{
-    console.log(a.length);
-}
-printLen("alok")
+// function printLen(a)
+// {
+//     console.log(a.length);
+// }
+// printLen("alok")
 
-// sum with default values
-function sum2 (a, b =20)
-{
-     console.log(a+b)
-}
- sum2(10,50) //60
-sum2(10);  //30
+// // sum with default values
+// function sum2 (a, b =20)
+// {
+//      console.log(a+b)
+// }
+//  sum2(10,50) //60
+// sum2(10);  //30
 
-//sum with rest
-function sum3 (...b)
+// //sum with rest
+// function sum3 (...b)
+// {
+//     let sum = 0;
+//     for (let i of b)
+//     {
+//         sum = sum + i;
+//     }
+//     console.log(sum)
+// }
+// sum3(10, 5, 4, 6, 3)
+// // take an array and pass multiple values to function and print their product
+// function sum3(...b) {
+// 	let sum = 0
+// 	for (let i of b) {
+// 		sum = sum * i
+// 	}
+// 	console.log(sum)
+// }
+// sum3(10, 5, 4, 6, 3)
+
+// function sum4 (a, ...b)
+// {
+//     console.log(a, b)
+//     let c = [a, b];  // [5,4,3,2]
+//     console.log(c,c.length)
+// }
+// sum4(5,4,3,2)
+
+// let [a, ...c] = [6, 7, 8, 9, 10];
+// let b=[5,...[4,3],...[5,4],3]
+// console.log(b)
+
+// // add two arrays
+// let arr = [5, 4, 6], arr2 = [1, 2, 3];
+// let arr3 = [...arr,...arr2]
+
+
+// function sum (a,b)
+// {
+//     return a + b;
+// }
+// let res = sum(4, 5)
+// console.log(res)
+
+// //function expression
+// const x = 2;
+// const sum2 = function (a, b)
+// {
+//     return a + b
+// }
+// console.log(sum2)
+// let res2 = sum2(4, 5)
+// console.log(res)
+
+
+
+
+// function display ()
+// {
+//     console.log("hellllo world")
+// }
+// console.log(display)
+// display()
+
+
+// const x = function ()
+// {
+//     console.log("hello world")
+// }
+// console.log(x)
+// x()
+
+// function sum (a, b)
+// {
+//     return a + b;
+// }
+// console.log(sum)
+// // function expression
+// let x = function (a, b)
+// {
+//     return a + b;
+// }
+// console.log(x)
+// // arrow function
+// let y = (a, b) => a+b
+
+// console.log(y)
+
+
+
+
+
+
+
+
+
+
+
+// function strLen (a)
+// {
+//     return a.length;
+// }
+// console.log(strLen("prasad"))
+// // arrow function
+// const z = (a) => a.length;
+// console.log(z("prasad"))
+
+
+
+function x (p,q)
 {
-    let sum = 0;
-    for (let i of b)
-    {
-        sum = sum + i;
+    console.log("i'm  x"+(p+q))
+}
+function doPrint (a)
+{
+    a(5,3)
+}
+doPrint(x) // x is callback and doPrint is Higher order function
+
+
+//closure
+function outer ()
+{
+    let x = 10;
+    function inner(){
+        x++;
+        console.log(x)
     }
-    console.log(sum)
+    return inner;
 }
-sum3(10, 5, 4, 6, 3)
-// take an array and pass multiple values to function and print their product
-function sum3(...b) {
-	let sum = 0
-	for (let i of b) {
-		sum = sum * i
-	}
-	console.log(sum)
-}
-sum3(10, 5, 4, 6, 3)
+let z = outer()  // let z = inner
+console.log(z)
+z() //11
+z() //12
+z() //13
 
-function sum4 (a, ...b)
+
+// funny closure
+function rani ()
 {
-    console.log(a, b)
-    let c = [a, b];  // [5,4,3,2]
-    console.log(c,c.length)
+    let msg="bye bye"
+    function letter ()
+    {
+        console.log(msg)
+    }
+    return letter;
 }
-sum4(5,4,3,2)
+  let y = rani() /*
+   let y = function letter ()
+    {
+        console.log(msg)
+    }*/
+y()
 
-let [a, ...c] = [6, 7, 8, 9, 10];
-let b=[5,...[4,3],...[5,4],3]
-console.log(b)
 
-// add two arrays
-let arr = [5, 4, 6], arr2 = [1, 2, 3];
-let arr3 = [...arr,...arr2]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
